9a24dcfd0028fef11665bc005d402861f8095fc2
eliasmelgaco <eliasmelgaco@gmail.com>
2021-01-28 17:53:41 -0300

    improve

--- src/get-latest-pull-request.js
+++ src/get-latest-pull-request.js
@@ -5,6 +5,8 @@ const GitHubCore = require('@actions/core');
 module.exports = async (Octokit, owner, repo) => {
   const { data: releases } = await Octokit.repos.listReleases({ owner, repo });
 
+  const [latestRelease] = releases;
+
   GitHubCore.info('inside get-lated-pull-request.js');
   // GitHubCore.info(releases);
   // GitHubCore.info(releases[0]);
@@ -16,6 +18,10 @@ module.exports = async (Octokit, owner, repo) => {
   });
 
   GitHubCore.info(properties);
+  GitHubCore.info(latestRelease.tag_name);
+  GitHubCore.info(latestRelease.name);
+  GitHubCore.info(latestRelease.body);
+  GitHubCore.info(latestRelease.url);
 
   return releases[0];
 };
