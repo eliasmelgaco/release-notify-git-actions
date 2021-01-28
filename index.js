4132820726d10f12cb47a2105bfaf24161a25530
eliasmelgaco <eliasmelgaco@gmail.com>
2021-01-28 17:41:15 -0300

    improve

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function run () {
 
     message = await getSlackMessage(Octokit, owner, repo);
 
-    GitHubCore.debug(message);
+    GitHubCore.info(message);
 
 
     GitHubCore.info('Message built');
