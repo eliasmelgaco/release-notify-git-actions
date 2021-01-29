e88a17eb964243d7689b81809903c8cb0b2c5853
eliasmelgaco <eliasmelgaco@gmail.com>
2021-01-29 11:19:31 -0300

    improve

--- src/get-latest-pull-request.js
+++ src/get-latest-pull-request.js
@@ -15,15 +15,18 @@ module.exports = async (Octokit, owner, repo) => {
 
   // GitHubCore.info('inside get-lated-pull-request.js');
 
-  // let properties = '';
-  // Object.getOwnPropertyNames(releases[0]).forEach((val) => {
-  //   properties += `,${val}`;
-  // });
+  let properties = '';
+  Object.getOwnPropertyNames(releases[0]).forEach((val) => {
+    properties += `,${val}`;
+  });
 
-  GitHubCore.info(latestRelease.tag_name); // v0.0.29
-  GitHubCore.info(latestRelease.name); // title 321321
-  GitHubCore.info(latestRelease.body); // description 123 123
-  GitHubCore.info(latestRelease.html_url); // https://github.com/PayCertify/release-notify-git-actions/releases/tag/v0.0.29
+  GitHubCore.info(properties); // v0.0.29
+
+
+  // GitHubCore.info(latestRelease.tag_name); // v0.0.29
+  // GitHubCore.info(latestRelease.name); // title 321321
+  // GitHubCore.info(latestRelease.body); // description 123 123
+  // GitHubCore.info(latestRelease.html_url); // https://github.com/PayCertify/release-notify-git-actions/releases/tag/v0.0.29
 
   return {
     url,
